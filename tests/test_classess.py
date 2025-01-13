1df42df0b36e2f100cf40cca5d4945523be83fcd
Gordey <gordej.taran@mail.ru>
2025-01-13 18:23:11 +0500

    home_work_fix

--- tests/test_classess.py
+++ tests/test_classess.py
@@ -1,9 +1,4 @@
-from src.classess import Product, Category
-import pytest
-
-@pytest.fixture
-def product_apple():
-    return Product("Apple", "red", 99.9, 1000)
+from src.category import Category
 
 
 def test_init_product(product_apple):
@@ -11,11 +6,6 @@ def test_init_product(product_apple):
     assert product_apple.description == "red"
     assert product_apple.price == 99.9
     assert product_apple.quantity == 1000
-
-
-@pytest.fixture
-def category_fruit():
-    return Category("fruits", "fruits from India", ["banana", "mango"])
 
 
 def test_init_category(category_fruit):
